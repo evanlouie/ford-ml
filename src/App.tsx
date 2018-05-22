9a88414351f3ec05b16b77c0e3a6ab7fbdc88c04
Evan Louie <evan.louie@microsoft.com>
2018-05-22 15:21:58 -0400

    basic links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Route, RouteComponentProps } from "react-router-dom";
+import { BrowserRouter, Link, Route, RouteComponentProps } from "react-router-dom";
 import "./App.css";
 import { JobForm } from "./components/JobForm";
 import { ModelList } from "./components/ModelList";
@@ -14,18 +14,20 @@ export class App extends React.Component {
 
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Ford ML</h1>
-        </header>
-        <BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to Ford ML</h1>
+          </header>
           <div>
+            <Link to="/">Submit Training Job</Link>
+            <Link to="/model">Trained Models</Link>
             {Object.entries(App.exactRoutes).map(([route, component]) => {
               return <Route key={route} exact path={route} component={component} />;
             })}
           </div>
-        </BrowserRouter>
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
